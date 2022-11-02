80b11f86cb3f93c6d31e2ac2929c9d736691bf03
Kasey <kasey_crocker@yahoo.com>
2022-11-02 16:46:52 -0400

    handlebars structured

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const PORT = process.env.PORT || 3001;
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 
 const sess = {
@@ -30,4 +31,4 @@ app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
-});
+}); 
