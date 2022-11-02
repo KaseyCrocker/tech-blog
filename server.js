f0109a889de592592db02ad81d9b7d35def6331f
Kasey <kasey_crocker@yahoo.com>
2022-11-02 15:51:55 -0400

    user routes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//const routes = require('./controllers/');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
 const app = express();
@@ -26,7 +26,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
