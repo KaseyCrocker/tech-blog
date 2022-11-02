934479bdcbaa6e8a8143ad9d2ebd199f43e28e9e
Kasey <kasey_crocker@yahoo.com>
2022-11-02 15:24:43 -0400

    models

---
+++ models/Post.js
@@ -0,0 +1,44 @@
+// sequelize model, datatypes, and database connection
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+// define the table columns and configuration, similar to the setup for the User model
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isURL: true
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+)
+
+// Export the model
+module.exports = Post;
